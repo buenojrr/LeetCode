95f90552b3e87d3f3c1f265bdd37c20d3c51d547
Alcides Bueno Jr <bueno-jr@hotmail.com>
2022-06-21 16:50:36 -0300

    Add files via upload

---
+++ TwoSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp
+{
+  class TwoSum
+  {
+
+    public static void Main(String[] args)
+    {
+      int[] novo = new int[] { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
+
+      int[] result = findIndex(novo, 11);
+
+
+      Console.WriteLine("[" + string.Join(",", result) + "]");
+    }
+
+    public static int[] findIndex(int[] nums, int target)
+    {
+      Dictionary<int, int> map = new Dictionary<int, int>();
+
+      int len = nums.Length;
+      for (int i = 0; i < len; i++)
+      {
+        int complement = target - nums[i];
+
+        if (map.ContainsKey(complement))
+        {
+          return new int[] { map.GetValueOrDefault(complement), i };
+        }
+        map.TryAdd(nums[i], i);
+      }
+      return new int[] { };
+    }
+  }
+}
